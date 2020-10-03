0b3405f9ae98e7595ab067ccbcfac32ce57a496d
John Bolognino <jcbolo72012@gmail.com>
2020-10-03 15:15:50 -0400

    Added login form functionality

--- timebankDjangodb/timebank/models.py
+++ timebankDjangodb/timebank/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Member(models.Model):
+    fname = models.CharField(max_length = 50)
+    lname = models.CharField(max_length = 100)
+    email = models.EmailField(max_length = 200)
+    passwd = models.CharField(max_length = 50)
+    age = models.IntegerField()
+    credits = models.IntegerField()
+    cname = models.CharField(max_length = 100)
+    def __str__(self):
+        return self.fname + ' ' + self.lname
+
+class Community(models.Model):
+    cname = models.CharField(max_length = 100)
+    members = models.IntegerField
